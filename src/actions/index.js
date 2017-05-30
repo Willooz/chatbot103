42904d54282553ac96c8dfa3e1796a973413bf12
Willem <willem.browne@gmail.com>
2017-05-30 20:57:55 +0200

    added actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,6 @@
 const actions = {
   'trending-stars': require('./trending-stars'),
   'trending-forks': require('./trending-forks'),
-  'show-language': require('./show-language'),
   'laugh': require('./laugh'),
 }
 
@@ -9,7 +8,11 @@ export default async function handleAction(res, payload) {
   const currentAction = res.action && res.action.slug
   console.log(currentAction)
   let replies = []
-  if (res.reply()) {
+  /* call to services */
+  if (actions[currentAction]) {
+    console.log('Enter action')
+    replies = await actions[currentAction].default(res, payload)
+  } else if (res.reply()) {
     replies.push({
       type: 'text',
       content: res.reply(),
